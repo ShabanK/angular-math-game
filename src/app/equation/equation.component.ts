3430f0e9e8841ecf0044527fbe01f0b019711729
ShabanK <shabankhawar99@gmail.com>
2021-01-26 12:54:20 +0500

    completed validators

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators, AbstractControl, MaxLengthValidator} from "@angular/forms"
+import { CustomValidators } from "../custom-validators"
 
 @Component({
   selector: 'app-equation',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquationComponent implements OnInit {
 
+  mathForm = new FormGroup({
+    a: new FormControl(this.generateRandomNumber()),
+    b: new FormControl(this.generateRandomNumber()),
+    answer: new FormControl("")},
+    [
+      CustomValidators.addition("answer", "a", "b")
+    ]                 
+  )
+
   constructor() { }
 
-  ngOnInit() {
+  //getters
+  get a(){
+    return this.mathForm.value.a
   }
 
+  get b(){
+    return this.mathForm.value.b
+  }
+
+  ngOnInit() {
+    this.mathForm.statusChanges.subscribe((value)=>{
+      if(value==="INVALID"){
+        return;
+      } else {
+        this.mathForm.patchValue({
+          a:this.generateRandomNumber(),
+          b:this.generateRandomNumber(),
+          answer: ""
+        })
+        // this.mathForm.controls.a.setValue(this.generateRandomNumber())
+        // this.mathForm.controls.b.setValue(this.generateRandomNumber())
+        // this.mathForm.controls.answer.setValue("")
+      }
+    })
+
+  }
+  generateRandomNumber(){
+    return Math.floor(Math.random()*10) //return number b/w 0 and 9
+  }
 }
